9fe0389db943c430efad2f3d2c9d6f2ab5a65624
NousheenKhhan <mwasikhan681@gmail.com>
2024-11-22 12:27:28 -0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Hello() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 text-blue-500 text-2xl font-bold">
+        HELLO WORLD
+      </div>
+    );
+  };
+  
